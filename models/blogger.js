59b3d556728a76946208b9f366084ad40a479827
michaelclancy90 <michaelclancy@hotmail.co.uk>
2022-09-05 20:24:57 +1000

    blogger model updated

---
+++ models/blogger.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+Blogger.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    content: {
+      type: DataTypes.TEXT,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blogger',
+  }
+);
