498a479ae3a853f33935a7f20b54fdd6746b2b5e
michaelclancy90 <michaelclancy@hotmail.co.uk>
2022-09-05 22:20:48 +1000

    updated index model. debugged the rest of the models

--- models/index.js
+++ models/index.js
@@ -2,9 +2,28 @@ const Blogger = require('./blogger');
 const Blog = require('./blog');
 const Comment = require('./comment');
 
-/*Blog.belongsTo(Blogger, {
-  foreignKey,
-});*/
+Blog.belongsTo(Blogger, {
+  foreignKey: 'blogger_id',
+});
+
+Comment.belongsTo(Blogger, {
+  foreignKey: 'blogger_id',
+});
+
+Comment.belongsTo(Blog, {
+  foreignKey: 'Blog_id',
+});
+
+Blogger.hasMany(Blog, {
+  foreignKey: 'blogger_id',
+});
+
+Blogger.hasMany(Comment, {
+  foreignKey: 'blogger_id',
+});
+Blog.hasMany(Comment, {
+  foreignKey: 'Blog_id',
+});
 
 module.exports = {
   Blogger,
